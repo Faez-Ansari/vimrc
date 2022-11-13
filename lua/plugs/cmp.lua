5205d56768beb73bd99738d1d530ff611cf799d6
Faez Ansari <faez@psight.io>
2022-11-13 15:37:00 +0400

    Adding plugs

---
+++ lua/plugs/cmp.lua
@@ -0,0 +1,81 @@
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+local select_opts = { behavior = cmp.SelectBehavior.Select }
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	sources = {
+		{ name = "path" },
+		{ name = "TabNine" },
+		{ name = "nvim_lsp", keyword_length = 3 },
+		{ name = "buffer", keyword_length = 3 },
+		{ name = "luasnip" },
+	},
+	window = {
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = {
+		["<C-k>"] = cmp.mapping.select_prev_item(select_opts),
+		["<C-j>"] = cmp.mapping.select_next_item(select_opts),
+
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm(),
+
+		["<C-d>"] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(1) then
+				luasnip.jump(1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<C-b>"] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	},
+})
+
+require("luasnip.loaders.from_vscode").lazy_load()
+local tabnine = require("cmp_tabnine.config")
+tabnine.setup({ max_lines = 1000, max_num_results = 20, sort = true })
---
+++ lua/plugs/init.lua
@@ -0,0 +1,4 @@
+require("plugs/lsp")
+require("plugs/nullLs")
+require('plugs/cmp')
+require('plugs/treesitter')
