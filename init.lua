9bd03f3762abe4c76884dd8b31a714c37acbef9d
Faez <faez@psight.io>
2023-05-13 12:39:04 +0400

    adding init.lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+require("plugins")
+require("binds")
+require("colorscheme")
+require("vimset")
+require("plugs")
+require("nvim-ts-autotag").setup()
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("mini.pairs").setup()
