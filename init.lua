7ab7cab77ead435d61538dcb6e8fd0628fb8ba8b
Faez <faez@psight.io>
2023-05-29 00:48:31 +0400

    More clean up

--- init.lua
+++ init.lua
@@ -5,7 +5,13 @@ require("vimset")
 require("plugs")
 require("nvim-ts-autotag").setup()
 
+
 require("mason").setup()
 require("mason-lspconfig").setup()
 
 require("mini.pairs").setup()
+require('mru_cache').setup({
+  max_size = 1000,
+  ignore_filetype_list = { "help" },
+  ignore_regex_list = { "%.git/" }
+})
